a395f42840a3cb85375cce6636a6422440ab5cd8
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-03-06 17:21:02 +0530

    ScrollUpAndDown function added

--- src/test/java/Selenium/JavaScriptExecuter.java
+++ src/test/java/Selenium/JavaScriptExecuter.java
@@ -22,4 +22,23 @@ public class JavaScriptExecuter {
         // updating disabled textfield text
         js.executeScript("document.getElementById('t2').value='Zodge'");
     }
+
+    @Test
+    public void ScrollUpAndDown() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://seleniumhq.org/download");
+        //typecasting driver object to JavascriptExecutor interface type
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for (int i = 1; i < 10; i++) {
+            //scroll down on the webpage
+            js.executeScript("window.scrollBy(0, 1000)");
+            Thread.sleep(3000);
+        }
+        for (int i = 1; i < 10; i++) {
+            //scroll up on the webpage
+            js.executeScript("window.scrollBy(0, -1000)");
+            Thread.sleep(3000);
+        }
+    }
 }
