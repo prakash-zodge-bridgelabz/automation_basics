92af67895f748c1554b1de2a33d6bd6aba7d745e
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-03-08 11:39:23 +0530

    Added: Upload pdf file through AutoIT and getChromeID

--- src/test/java/Selenium/filePopUp.java
+++ src/test/java/Selenium/filePopUp.java
@@ -27,5 +27,10 @@ public class filePopUp {
         Runtime.getRuntime().exec("D:\\Quality Kiosk SDET\\Application Interface & E2E QA\\au3 files\\Polymorphism_pdf.exe");
         Thread.sleep(3000);
     }
-
+    @Test
+    public void getChromeID(){
+        driver.get("https://leetcode.com/user4417TL/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getWindowHandle());
+    }
 }
